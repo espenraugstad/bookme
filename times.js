e1d68bf39d7dc99b1a0ef22752d3e9d74ca0e96e
espenraugstad <espen.raugstad@gmail.com>
2023-11-03 12:37:26 +0100

    Some intial tests

---
+++ times.js
@@ -0,0 +1,6 @@
+export const times = [
+    {dateStart: new Date("2023-11-07T09:00:00")},
+    {dateStart: new Date("2023-11-07T10:00:00")},
+    {dateStart: new Date("2023-11-07T11:00:00")},
+    {dateStart: new Date("2023-11-07T12:00:00")}
+];
