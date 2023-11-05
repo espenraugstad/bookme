4074313c0f37cd0e4cfd3a4be941be1504b3345f
espenraugstad <espen.raugstad@gmail.com>
2023-11-05 01:10:17 +0100

    Selects 35 days every month, starting with mondays, and covering all of the selected month

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -15,6 +15,7 @@ const months = [
 
   const month = document.getElementById('month');
   const year = document.getElementById('year');
+  const calendar = document.getElementById('calendar');
 
   month.addEventListener("change", showCalendar);
   year.addEventListener("change", showCalendar);
@@ -22,23 +23,41 @@ const months = [
   
 
   function showCalendar(){
-    let thisMonth = [];
+    calendar.innerHTML = "<span>Mandag</span><span>Tirsdag</span><span>Onsdag</span><span>Torsdag</span><span>Fredag</span><span>Lørdag</span><span>Søndag</span>";
+    let thisMonth = generateDays();
+    console.log(thisMonth);
+    for(const day of thisMonth){
+      const div = document.createElement("div");
+      div.innerText = `${day.getDate()}.`;
+      calendar.appendChild(div);
+    }
+      
+  }
+
+  function generateDays(){
     // Get month
     const selectedMonthIndex = month.selectedIndex;
     
     // Get year
     const selectedYear = parseInt(year.value);
 
-    // Array of possible dates
-    let dates = [...Array(31).keys()];
+    // Array of dates
+    let dates = [...Array(35).keys()];
 
-    // Create an array of days for each possible date
-    let days = dates.map(day => new Date(selectedYear, selectedMonthIndex, day + 1));
+    /*
+      Weeks starts on mondays, so first date always needs to be a monday, regardless of whether it is in the current of prevoius month.
 
-    // Filter out any days not in this month
-    let daysOfThisMonth = days.filter(day => day.getMonth() === selectedMonthIndex);
+      So, what day of the week is the first of the selected month? x
+      Then we have to go 1 - x days back to get to the first monday.
+      If we generate dates from day + 1, then we subtract |1-x| days
+    */
+    //console.log((new Date(selectedYear, selectedMonthIndex, 1)).getDay());    
+    let firstDayOfMonth = (new Date(selectedYear, selectedMonthIndex, 1)).getDay();
+    console.log(firstDayOfMonth);
+    let d = 0;
+    firstDayOfMonth === 0 ? d = -6 : d = 1 - firstDayOfMonth
+    // Create an array of days for each dates
+    let days = dates.map(day => new Date(selectedYear, selectedMonthIndex, (day + 1) + d));
     
-    
-    console.log(daysOfThisMonth);
-      
+    return days;
   }
